d62f6443e90ac4f2b6ea10a8fc15a2187ea544e8
viralhysteria <1983431+viralhysteria@users.noreply.github.com>
2023-08-10 07:37:07 -0500

    revert: nextconfig (webpackDevMiddleware)

--- next.config.js
+++ next.config.js
@@ -3,19 +3,11 @@
 const path = require("path");
 
 module.exports = {
-  cssModules: true,
   distDir: "build",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.alias['components'] = path.join(__dirname, 'src/components');
     }
     return config;
-  },
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
+  }
 };
