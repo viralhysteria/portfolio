ab5f60cf75fb7232653af619589ea555d55e0b85
viralhysteria <1983431+viralhysteria@users.noreply.github.com>
2023-08-27 19:59:01 -0500

    fix: use react state to manage bubble query styles

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,16 +4,11 @@ import styles from "@/styles/skills.module.css";
 import { randColor } from "@/utils/randColor";
 
 function Bubble(Properties) {
+  const [bubbleStyle, setBubbleStyle] = useState({});
+  const [iconStyle, setIconStyle] = useState({});
   const [starColor, setStarColor] = useState([]);
 
-  useEffect(() => {
-    import("bootstrap/js/src/modal");
-    const colors = Array.from({ length: Properties.rank }, () => randColor());
-    setStarColor(colors);
-  }, [Properties.rank]);
-
   const rank = "★".repeat(Properties.rank);
-
   const stars = Array.from(rank).map((item, i) => {
     const style = { color: starColor[i] };
     return (
@@ -23,21 +18,84 @@ function Bubble(Properties) {
     );
   });
 
+  useEffect(() => {
+    import("bootstrap/js/src/modal");
+    const colors = Array.from({ length: Properties.rank }, () => randColor());
+    setStarColor(colors);
+  }, [Properties.rank]);
+
+  useEffect(() => {
+    const mediaQueries = [
+      { minWidth: 0, maxWidth: 767 },
+      { minWidth: 768, maxWidth: 991 },
+      { minWidth: 992, maxWidth: 1920 },
+      { minWidth: 1921, maxWidth: 3780 },
+      { minWidth: 800, maxWidth: 9999, minHeight: 1921 },
+    ];
+
+    const handleMediaQueryChange = (mediaQueryList, index) => {
+      if (mediaQueryList.matches) {
+        const bubbleWidth = 80 + index * 20;
+        const iconFontSize = 1 + index * 0.2;
+
+        setBubbleStyle({
+          width: `${bubbleWidth}px`,
+          height: `${bubbleWidth}px`,
+          background: Properties.color,
+        });
+
+        setIconStyle({
+          fontSize: `${iconFontSize}rem`,
+        });
+      }
+    };
+
+    mediaQueries.forEach((query, index) => {
+      const { minWidth, maxWidth, minHeight } = query;
+      const mediaQuery = window.matchMedia(
+        `(min-width: ${minWidth}px) and (max-width: ${maxWidth}px)${
+          minHeight ? ` and (min-height: ${minHeight}px)` : ""
+        }`
+      );
+
+      mediaQuery.addEventListener("change", () =>
+        handleMediaQueryChange(mediaQuery, index)
+      );
+      handleMediaQueryChange(mediaQuery, index);
+    });
+
+    return () => {
+      mediaQueries.forEach((query, index) => {
+        const { minWidth, maxWidth, minHeight } = query;
+        const mediaQuery = window.matchMedia(
+          `(min-width: ${minWidth}px) and (max-width: ${maxWidth}px)${
+            minHeight ? ` and (min-height: ${minHeight}px)` : ""
+          }`
+        );
+
+        mediaQuery.removeEventListener("change", () =>
+          handleMediaQueryChange(mediaQuery, index)
+        );
+      });
+    };
+  }, [Properties.color]);
+
   return (
     <div className="col-4">
       <div
         className={`${styles.bubble} hvr-pulse select`}
         data-bs-html="true"
-        style={{ background: `${Properties.color}` }}
+        style={{ ...bubbleStyle, background: `${Properties.color}` }}
       >
         {Properties.body ? (
           <i
             className={`${Properties.icon}`}
             data-bs-toggle="modal"
             data-bs-target={`#${Properties.id}Modal`}
+            style={iconStyle}
           ></i>
         ) : (
-          <i className={`${Properties.icon}`}></i>
+          <i className={`${Properties.icon}`} style={iconStyle}></i>
         )}
       </div>
       {Properties.body ? (
--- src/script.js
+++ src/script.js
@@ -43,44 +43,4 @@ if (typeof window !== "undefined") {
     removeClassesOnAnimationEnd(skills, ["animated", "animate__fadeInUpBig"]);
     removeClassesOnAnimationEnd(nametag, ["animated", "animate__zoomIn"]);
   });
-
-  // bubble dimension handler
-  const bubbles = document.querySelectorAll('[class^="skills_bubble"]');
-
-  const mediaQueries = [
-    { minWidth: 0, maxWidth: 767 },
-    { minWidth: 768, maxWidth: 991 },
-    { minWidth: 992, maxWidth: 1920 },
-    { minWidth: 1921, maxWidth: 3780 },
-    { minWidth: 800, maxWidth: 9999, minHeight: 1921 },
-  ];
-
-  const handleMediaQueryChange = (mediaQueryList, index) => {
-    if (mediaQueryList.matches) {
-      const bubbleWidth = 80 + index * 20;
-
-      bubbles.forEach((bubble) => {
-        const icon = bubble.querySelector("i");
-        const iconFontSize = 1.0 + index * 0.2;
-
-        bubble.style.width = `${bubbleWidth}px`;
-        bubble.style.height = `${bubbleWidth}px`;
-        icon.style.fontSize = `${iconFontSize}rem`;
-      });
-    }
-  };
-
-  mediaQueries.forEach((query, index) => {
-    const { minWidth, maxWidth, minHeight } = query;
-    const mediaQuery = window.matchMedia(
-      `(min-width: ${minWidth}px) and (max-width: ${maxWidth}px)${
-        minHeight ? ` and (min-height: ${minHeight}px)` : ""
-      }`
-    );
-
-    mediaQuery.addEventListener("change", () =>
-      handleMediaQueryChange(mediaQuery, index)
-    );
-    handleMediaQueryChange(mediaQuery, index);
-  });
 }
