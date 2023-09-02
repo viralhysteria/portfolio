15ed63bce80cf876d861b7d2619085762b8e3814
viralhysteria <1983431+viralhysteria@users.noreply.github.com>
2023-09-02 18:04:59 -0500

    style: updated brand design

--- src/components/Float.jsx
+++ src/components/Float.jsx
@@ -58,7 +58,7 @@ export default function QuickMenu() {
   // const handleCloseModal = () => {
   //   setIsModalOpen(false);
   // };
-  
+
   return (
     <motion.header
       className="m-auto"
@@ -71,156 +71,154 @@ export default function QuickMenu() {
         ease: "easeInOut",
       }}
     >
-      <div style={{ pointerEvents: "auto" }}>
-        <ul
+      <ul
           className={`${styles.nav} nav navbar-expand-md fw-bold py-2 px-2 rounded-4 justify-content-around align-items-center`}
-        >
+      >
           <li className="nav-link px-2 select">
-            <Link href="/">
-              <FontAwesomeIcon icon={faHome} className={styles["nav-icon"]} />
-            </Link>
-          </li>
+          <Link href="/">
+            <FontAwesomeIcon icon={faHome} className={styles["nav-icon"]} />
+          </Link>
+        </li>
           <li className="nav-link px-2 select">
+          <FontAwesomeIcon
+            icon={faCircleUser}
+            className={styles["nav-icon"]}
+            style={{ userSelect: "none" }}
+            data-bs-toggle="modal"
+            data-bs-target="#bioModal"
+            // onClick={handleOpenModal}
+          />
+        </li>
+          <li className="nav-link px-2 select">
+          <Link href="/blog">
             <FontAwesomeIcon
-              icon={faCircleUser}
+              icon={faNewspaper}
               className={styles["nav-icon"]}
-              style={{ userSelect: "none" }}
-              data-bs-toggle="modal"
-              data-bs-target="#bioModal"
-              // onClick={handleOpenModal}
             />
-          </li>
-          <li className="nav-link px-2 select">
-            <Link href="/blog">
-              <FontAwesomeIcon
-                icon={faNewspaper}
-                className={styles["nav-icon"]}
-              />
-            </Link>
-          </li>
-          <motion.li
+          </Link>
+        </li>
+        <motion.li
             className="nav-link px-2 select"
-            initial=""
-            animate=""
-            variants=""
-            transition={{
-              delay: 4,
-              duration: 1,
-              ease: "easeOut",
-            }}
-            ref={ref}
+          initial=""
+          animate=""
+          variants=""
+          transition={{
+            delay: 4,
+            duration: 1,
+            ease: "easeOut",
+          }}
+          ref={ref}
+        >
+          <input
+            type="checkbox"
+            id="toggle"
+            className={styles["theme-toggle"]}
+            checked={theme === "dark"}
+            onChange={toggleTheme}
+          />
+          <label
+            htmlFor="toggle"
+            className={styles["theme-label"]}
+            style={{ userSelect: "none" }}
           >
-            <input
-              type="checkbox"
-              id="toggle"
-              className={styles["theme-toggle"]}
-              checked={theme === "dark"}
-              onChange={toggleTheme}
+            <FontAwesomeIcon
+              icon={theme === "dark" ? faSun : faCircle}
+              className={styles["nav-icon"]}
             />
-            <label
-              htmlFor="toggle"
-              className={styles["theme-label"]}
-              style={{ userSelect: "none" }}
-            >
-              <FontAwesomeIcon
-                icon={theme === "dark" ? faSun : faCircle}
-                className={styles["nav-icon"]}
-              />
-            </label>
-          </motion.li>
+          </label>
+        </motion.li>
 
-          <div id="bioModal" className="modal fade" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-              <div className="modal-content">
-                <motion.div
-                  className="modal-header justify-content-center"
-                  initial="0"
-                  animate="1"
-                  variants={zoomIn}
-                  transition={{
-                    delay: 5,
-                    duration: 2,
-                    ease: "easeInOut",
-                  }}
-                >
-                  <Image
-                    className="img-fluid w-100 nametag"
-                    src="/img/nametag.png"
-                    alt=""
-                    width="600"
-                    height="120"
-                  />
-                </motion.div>
-                <div className="modal-body fw-normal rounded mx-3">
-                  <p className="about-text">
-                    I am a dedicated full-stack developer with over 15 years of
-                    experience in the IT industry. Throughout my life, I have
+        <div id="bioModal" className="modal fade" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div className="modal-content">
+              <motion.div
+                className="modal-header justify-content-center"
+                initial="0"
+                animate="1"
+                variants={zoomIn}
+                transition={{
+                  delay: 5,
+                  duration: 2,
+                  ease: "easeInOut",
+                }}
+              >
+                <Image
+                  className="img-fluid w-100 nametag"
+                  src="/img/brand.png"
+                  alt=""
+                  width="1750"
+                  height="300"
+                />
+              </motion.div>
+              <div className="modal-body fw-normal rounded mx-3">
+                <p className="about-text">
+                  I am a dedicated full-stack developer with over 15 years of
+                  experience in the IT industry. Throughout my life, I have
                     worked with various operating systems, front-end and
                     back-end frameworks, programming languages, and software
                     applications. My passion for technology drives me to stay
                     updated with the latest advancements in the field.
-                    <br />
-                    <br />
-                    Beyond my professional life, I have a keen interest in
-                    intellectual pursuits, particularly in the fields of
+                  <br />
+                  <br />
+                  Beyond my professional life, I have a keen interest in
+                  intellectual pursuits, particularly in the fields of
                     pharmacology, neuroscience, and finance. My knowledge
                     extends to cannabinoids, serotonin subtype agonists, and
                     other related subjects. However, my curiosity extends beyond
                     these topics as I constantly seek to expand my horizons.
-                    <br />
-                    <br />
-                    Additionally, I adopt a systematic approach to analyze and
-                    understand broader markets. Applying both fundamental and
-                    technical analysis, I engage in swing trading
+                  <br />
+                  <br />
+                  Additionally, I adopt a systematic approach to analyze and
+                  understand broader markets. Applying both fundamental and
+                  technical analysis, I engage in swing trading
                     cryptocurrencies. This multidisciplinary approach allows me
                     to combine my passion for technology with my interest in
                     finance.
-                  </p>
-                </div>
-                <motion.div
-                  className="modal-footer d-flex justify-content-end"
-                  initial="0"
-                  animate="1"
-                  variants={fadeIn}
-                  transition={{
-                    delay: 1,
-                    duration: 3,
-                    ease: "easeInOut",
-                  }}
-                  ref={ref}
-                >
-                  <ul className="nav">
-                    <a
-                      className="nav-link social-discord"
-                      href="https://discordapp.com/users/653346377010642944"
-                    >
-                      <FontAwesomeIcon icon={faDiscord} fixedWidth />
-                    </a>
-                    <a
-                      className="nav-link social-twitter"
-                      href="https://twitter.com/viralhysteria"
-                    >
-                      <FontAwesomeIcon icon={faTwitter} fixedWidth />
-                    </a>
-                    <a
-                      className="nav-link social-github"
-                      href="https://github.com/viralhysteria"
-                    >
-                      <FontAwesomeIcon icon={faGithub} fixedWidth />
-                    </a>
-                    <a
-                      className="nav-link social-twitch"
-                      href="https://twitch.tv/viralhysteria/about"
-                    >
-                      <FontAwesomeIcon icon={faTwitch} fixedWidth />
-                    </a>
-                  </ul>
-                </motion.div>
+                </p>
               </div>
+              <motion.div
+                className="modal-footer d-flex justify-content-end"
+                initial="0"
+                animate="1"
+                variants={fadeIn}
+                transition={{
+                  delay: 1,
+                  duration: 3,
+                  ease: "easeInOut",
+                }}
+                ref={ref}
+              >
+                <ul className="nav">
+                  <a
+                    className="nav-link social-discord"
+                    href="https://discordapp.com/users/653346377010642944"
+                  >
+                    <FontAwesomeIcon icon={faDiscord} fixedWidth />
+                  </a>
+                  <a
+                    className="nav-link social-twitter"
+                    href="https://twitter.com/viralhysteria"
+                  >
+                    <FontAwesomeIcon icon={faTwitter} fixedWidth />
+                  </a>
+                  <a
+                    className="nav-link social-github"
+                    href="https://github.com/viralhysteria"
+                  >
+                    <FontAwesomeIcon icon={faGithub} fixedWidth />
+                  </a>
+                  <a
+                    className="nav-link social-twitch"
+                    href="https://twitch.tv/viralhysteria/about"
+                  >
+                    <FontAwesomeIcon icon={faTwitch} fixedWidth />
+                  </a>
+                </ul>
+              </motion.div>
             </div>
           </div>
-        </ul>
-      </div>
+        </div>
+      </ul>
     </motion.header>
   );
 }
