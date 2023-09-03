cb04706bd788ec07ddf3ceff17355c58c975a980
viralhysteria <1983431+viralhysteria@users.noreply.github.com>
2023-09-02 19:50:18 -0500

    fix: quickmenu false rendering on blog posts

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,11 @@
+import QuickMenu from "@/components/Float";
 import DiscordMarquee from "@/components/Discord";
 
 const Layout = ({ children }) => {
   return (
     <>
       <div className="pattern position-absolute top-0 start-0 w-100 h-100"></div>
+      <QuickMenu />
       {children}
       <DiscordMarquee />
     </>
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Layout from "@/components/Layout";
-import QuickMenu from "@/components/Float";
+
 import { useEffect } from "react";
 import "devicon/devicon.min.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -24,10 +24,8 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico?v=2" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
-      <div className="pattern position-absolute top-0 start-0 w-100 h-100"></div>
-      <div className="d-flex">
-        <QuickMenu />
-      </div>
+
+      <div className="d-flex"></div>
       <Component {...pageProps} />
     </Layout>
   );
